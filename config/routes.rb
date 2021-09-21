f8c52d058d918c53dbbef9302d67acafbe5a779c
y.goto <y.goto@cybermissions.co.jp>
2021-09-21 15:27:40 +0900

    8.23まで完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # get 'users/new' as: 'signup'
 
   root 'static_pages#home'
@@ -18,6 +20,10 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   post 'signup', to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # users/1みたいなgetリクエストを実現する手段
   resources :users
 
