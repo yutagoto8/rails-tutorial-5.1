c24b9d2dfcbcb592aa0b86235057a60c110191e9
y.goto <y.goto@cybermissions.co.jp>
2021-09-22 18:33:50 +0900

    user一覧画面の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
   def show
@@ -20,6 +20,10 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def index
+    @users = User.all
   end
 
   def edit
