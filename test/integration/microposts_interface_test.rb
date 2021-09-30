446ec122911f000d13d36b8a1b2b743129e90287
y.goto <y.goto@cybermissions.co.jp>
2021-09-30 16:40:42 +0900

    演習も完了

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,15 +9,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type="file"]'
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
     end
     assert_select 'div#error_explanation'
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content,
+                                                   picture: picture } }
     end
-    assert_redirected_to root_url
+    assert @user.microposts.first.picture?
     follow_redirect!
     assert_match content, response.body
     assert_select 'a', text: 'delete'
