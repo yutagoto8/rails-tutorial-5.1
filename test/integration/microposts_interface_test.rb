ee1149f6dd9591a6b09a2bce7ac336d98d57b1e1
y.goto <y.goto@cybermissions.co.jp>
2021-09-30 13:02:52 +0900

    演習やったけどあってるか分からない

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -29,4 +29,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'a', text: 'delete', count: 0
   end
 
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    # #内のプログラムを確認
+    assert_match "#{@user.microposts.count} microposts" , response.body
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    # # ""内のプログラムを確認
+    assert_match "1 micropost", response.body
+  end
 end
